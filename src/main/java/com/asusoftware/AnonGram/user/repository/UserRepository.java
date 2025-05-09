4cb1b10a1960f8133033752ef54a5754c9323eb1
antonio <antonio.popa.dev@gmail.com>
2025-05-09 10:46:20 +0300

    Update

--- src/main/java/com/asusoftware/AnonGram/user/repository/UserRepository.java
+++ src/main/java/com/asusoftware/AnonGram/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.asusoftware.AnonGram.user.repository;
 
 import com.asusoftware.AnonGram.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,7 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByKeycloakId(UUID keycloakId);
+    @Query("SELECT u.alias FROM User u WHERE u.id = :id")
+    Optional<String> findAliasById(@Param("id") UUID id);
+
 }
