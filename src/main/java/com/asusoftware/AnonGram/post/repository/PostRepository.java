db2a1d15809eb1fd5453ac1dc6d29de1949a7535
antonio <antonio.popa.dev@gmail.com>
2025-05-12 09:34:41 +0300

    Added Post clean up scheduled

--- src/main/java/com/asusoftware/AnonGram/post/repository/PostRepository.java
+++ src/main/java/com/asusoftware/AnonGram/post/repository/PostRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -74,6 +75,8 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
             @Param("longitude") Double longitude,
             Pageable pageable
     );
+
+    int deleteByExpiresAtBefore(LocalDateTime now);
 
 
 }
